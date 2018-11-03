bc4b86a2bdd29d8ebc3885f7035134d1c6935f7c
mr <mr@mr-PC>
2018-11-03 14:02:41 +0530

    new servlet tulasi project

---
+++ servlets/src/com/servlet/Login.java
@@ -0,0 +1,22 @@
+package com.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class Login extends GenericServlet{
+
+	@Override
+	public void service(ServletRequest req, ServletResponse resp) throws ServletException, IOException {
+
+		System.out.println("iam in service method");
+		
+		
+		
+		
+	}
+
+}
